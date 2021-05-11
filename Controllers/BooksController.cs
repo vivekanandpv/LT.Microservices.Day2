f4db8ff4c42a7d4246cfd4eb43100ee4e171c1ec
vivek <vivek@athenaeum.in>
2021-05-11 16:06:06 +0530

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using LT.Microservices.Day2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.Microservices.Day2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        //  GET
+        //  Get All
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            return Ok(new List<Book>());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            return Ok(new Book { Id = id, NPages = 285, Price = 100, Title = "C# Programming" });
+        }
+
+        //  POST
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            return Created($"http://localhost:5000/api/books/{book.Id}", book);
+        }
+
+        //  PUT
+        [HttpPut]
+        public IActionResult Update(Book book)
+        {
+            return Ok();
+        }
+
+        //  DELETE
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.Microservices.Day2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int NPages { get; set; }
+        public double Price { get; set; }
+    }
+}
